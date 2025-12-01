1157b4111056e67231a0dae688d6f0de9f6c9bed
yeyeyellow <3268266694@qq.com>
2025-12-02 04:04:48 +0800

    维护了一下监视脚本

--- fileWatcher_script/main.py
+++ fileWatcher_script/main.py
@@ -1,3 +1,6 @@
+'''
+用于操作文件
+'''
 from pathlib import Path
 import shutil
 import time
@@ -5,7 +8,7 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 
-scriptName = 'main.py'
+SCIPT_NAME = 'main.py'
 FILE_CATEGORIES = {
     "图片": [".jpg", ".jpeg", ".png", ".gif", ".webp"],
     "文档": [".pdf", ".docx", ".doc", ".txt", ".xlsx",'.xls'],
@@ -13,51 +16,63 @@ FILE_CATEGORIES = {
     "压缩包": [".zip", ".rar", ".7z", ".tar"],
     '视频':['.mp4']
 }
-def classify(i:Path)-> None: 
-        if i.name == scriptName:
-            return
-        if i.is_file():
-            for k,v in FILE_CATEGORIES.items():
-                if i.suffix in v:
-                    CATEGORIES_path = Path(i.parent/k)
-                    CATEGORIES_path.mkdir(parents=True,exist_ok=True)
-                    target = Path(CATEGORIES_path/i.name)
-                    if target.exists():
-                        org_name = i.stem
-                        suffix = i.suffix
-                        index =1
-                        while True:
-                            new_target_name = f'{org_name}({index}){suffix}'
-                            new_target = Path(CATEGORIES_path/new_target_name)
-                            if new_target.exists():
-                                index+=1
-                            else:
-                                break
-                        shutil.move(i,new_target)
-                    else:
-                        shutil.move(i,target)
+
+def classify(i:Path)-> None:
+    """
+    按照后缀名分类文件
+    """
+    if i.name == SCIPT_NAME:
+        return
+    if i.is_file():
+        for k,v in FILE_CATEGORIES.items():
+            if i.suffix in v:
+                categories_path = Path(i.parent/k)
+                categories_path.mkdir(parents=True,exist_ok=True)
+                target = Path(categories_path/i.name)
+                if target.exists():
+                    org_name = i.stem
+                    suffix = i.suffix
+                    index =1
+                    while True:
+                        new_target_name = f'{org_name}({index}){suffix}'
+                        new_target = Path(categories_path/new_target_name)
+                        if new_target.exists():
+                            index+=1
+                        else:
+                            break
+                    shutil.move(i,new_target)
+                else:
+                    shutil.move(i,target)
 
 class MyHandler(FileSystemEventHandler):
+    """
+    文件监听的回调
+    """
     def on_created(self, event):
-        event_Path=Path(event.src_path)
+        event_path=Path(event.src_path)
         i=0
         while i<9:
             try:
-                classify(event_Path)
+                classify(event_path)
                 break
-            except Exception:
+            except PermissionError:
                 time.sleep(1)
                 i+=1
     def on_moved(self, event):
-        event_Path=Path(event.dest_path)
-        classify(event_Path)
+        event_path = Path(event.dest_path)
+        classify(event_path)
 
-def main(path,path_str):
-    for entry in path.iterdir():
+def main(watching_path,watching_path_str):
+    """
+    主函数
+
+    args:watching_path(Path) 接受Path类型的对象,watching_path_str(str)
+    """
+    for entry in watching_path.iterdir():
         classify(entry)
-    myHandler = MyHandler()
+    my_handler = MyHandler()
     observer = Observer()
-    observer.schedule(myHandler,path_str,recursive=False,)
+    observer.schedule(my_handler,watching_path_str,recursive=False,)
     observer.start()
     try:
         while True:
